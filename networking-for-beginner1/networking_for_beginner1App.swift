2d86931ce13f15a0cf6bb4c3c6786cce17cbedeb
jkwon-dev <kwonejk@gmail.com>
2023-10-05 11:57:30 +0800

    Initial Commit

---
+++ networking-for-beginner1/networking_for_beginner1App.swift
@@ -0,0 +1,17 @@
+//
+//  networking_for_beginner1App.swift
+//  networking-for-beginner1
+//
+//  Created by kwon eunji on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct networking_for_beginner1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
